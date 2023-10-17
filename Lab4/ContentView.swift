4e9941a3fa233db8bcd70bf18b4ee6b764718335
Mateusz Redestowicz <mati.red@gmail.com>
2023-10-17 12:59:42 +0200

    Initial Commit

---
+++ Lab4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab4
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab4/Lab4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab4App.swift
+//  Lab4
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
